4d3908adb1adf1fff2f52fc0a018b0f3e7c21418
Roman Pavlenko <rpavlenko.mail@gmail.com>
2025-03-07 00:04:02 +0100

    feat: initial commit

---
+++ Portals/0/rich-content/garnier/EAN/js/script.js
@@ -0,0 +1,17 @@
+(() => {
+  if (!document.querySelector('.swiper1.swiper-initialized')) {
+    const swiper = new Swiper('.es-swiper1', {
+      direction: 'horizontal',
+      loop: false,
+
+      pagination: {
+        el: '.es-swiper1-pagination',
+      },
+
+      navigation: {
+        nextEl: '.es-swiper1-button-next',
+        prevEl: '.es-swiper1-button-prev',
+      },
+    });
+  }
+})();
